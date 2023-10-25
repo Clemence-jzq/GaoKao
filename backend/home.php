2b1ff7ef075be17811c5506a04b5b29514e2e803
Clemence-jzq <zheqin.jiang@gmail.com>
2023-10-25 05:30:09 +0200

    get Province data
    为选择框获取省份名称

--- backend/home.php
+++ backend/home.php
@@ -1,4 +1,14 @@
 <?php
-$data = '北京';
+include_once 'config.php';
 
-echo $data;
+$sql = "SELECT * FROM Province ORDER BY name_pinyin";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+    // 输出数据
+    while ($row = mysqli_fetch_assoc($result)) {
+        $data[] = $row;
+    }
+}
+
+echo json_encode($data);
--- frontend/js/home.js
+++ frontend/js/home.js
@@ -1,5 +1,8 @@
 $(document).ready(function () {
+    // 获取中国所有省市，不包括港澳台
     $.get('backend/home.php', function (data) {
-        $('#province').append(`<option value="${data}">${data}</option>`);
+        JSON.parse(data).forEach(element => {
+            $('#province').append(`<option value="${element.id}">${element.name}</option>`);
+        });
     });
 });
